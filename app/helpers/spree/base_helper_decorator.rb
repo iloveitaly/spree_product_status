109e178f6757fbd86148a6fdd9a6ae2adf657f27
Michael Bianco <iloveitaly@gmail.com>
2012-10-30 16:45:08 -0400

    Added :exclude option to helper method

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,6 +1,11 @@
 Spree::BaseHelper.module_eval do
-  def product_statuses_display(product)
-    product.statuses.map do |status|
+  def product_statuses_display(product, opts = {})
+    statuses = product.statuses
+
+    statuses.reject! { |s| opts[:exclude].include? s.status.to_sym } if opts[:exclude]
+    # more options here..
+
+    statuses.map do |status|
       content_tag(:div, class: "product-status product-status-#{status.status.dasherize}") do
         content_tag(:h6, t("product_status.#{status.status}")) + 
         content_tag(:p, status.description)
